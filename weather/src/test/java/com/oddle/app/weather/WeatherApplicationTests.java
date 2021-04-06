4d00475a9a49713ff4776da4efca32da6e1c433d
Duc <vanduc2514@gmail.com>
2021-04-07 00:32:59 +0700

    [A] Integration Test for search request

--- weather/src/test/java/com/oddle/app/weather/WeatherApplicationTests.java
+++ weather/src/test/java/com/oddle/app/weather/WeatherApplicationTests.java
@@ -1,13 +1,47 @@
 package com.oddle.app.weather;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.any;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(
+		locations = "classpath:weather-integration-test.properties"
+)
 class WeatherApplicationTests {
+
+	@Autowired
+	MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
+		assertNotNull(mockMvc);
+	}
+
+	@Test
+	public void shouldReturnPayLoadWhenSearch() {
+		try {
+			mockMvc.perform(MockMvcRequestBuilders
+					.get("/api/current")
+					.param("city", "hanoi"))
+					.andExpect(status().isOk())
+					.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+					.andExpect(jsonPath("$.weather").exists());
+		} catch (Exception exception) {
+			fail();
+		}
 	}
 
 }
