6a93c6b48f4ddda6b7f7f40eefb1e97346535ed9
Hobas Matius <hobas.matius@traveloka.com>
2023-02-24 22:04:59 +0700

    add missing annotation for city repository

--- weather/src/main/java/com/oddle/app/weather/repository/CityRepository.java
+++ weather/src/main/java/com/oddle/app/weather/repository/CityRepository.java
@@ -2,6 +2,8 @@ package com.oddle.app.weather.repository;
 
 import com.oddle.app.weather.model.internalmapper.City;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CityRepository extends JpaRepository<City, String> {
 }
